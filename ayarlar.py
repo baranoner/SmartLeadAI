bb6ff46f3c9275de1a7c7583685cdb790e149fe6
Baran Öner <baranoner7@protonmail.com>
2026-08-16 00:28:41 +0200

    Dosya temeli atıldı

--- ayarlar.py
+++ ayarlar.py
@@ -8,3 +8,14 @@ class Ayarlar:
     AI_PROVIDER = os.environ.get('AI_PROVIDER', 'groq')
     BUSINESS_CONTEXT = os.environ.get('BUSINESS_CONTEXT', '...')
     CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', '*')
+
+class GelistirmeAyarlari(Ayarlar):
+    DEBUG = True
+
+class UretimAyarlari(Ayarlar):
+    DEBUG = False
+
+ayar_secici = {
+    "gelistirme": GelistirmeAyarlari,
+    "uretim": UretimAyarlari,
+}
